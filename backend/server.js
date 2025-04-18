8074a1672a5d382607d209e087e5defbb83b4c77
ps982182 <singhal.riya2018@gmail.com>
2025-04-18 15:46:52 +0530

    Made changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-import app from './app.js';
+// import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on Port ${process.env.PORT}`)
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is running on Port ${process.env.PORT}`)
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
